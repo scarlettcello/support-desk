16ce86c06db6e9e56fef753a90c336b523da2d8e
scarlettcello <scarlettcello@gmail.com>
2022-06-06 01:57:47 -0400

    Setup DB and create register, login, and private routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log('Server ...');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 8000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome'});
+});
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () =>  console.log(`Server started on port ${PORT}`));

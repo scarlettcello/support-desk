f294b32f7767b3a1887eb92e94d309f91e92aca5
scarlettcello <scarlettcello@gmail.com>
2022-07-16 22:53:38 -0400

    Prepared for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -12,12 +13,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome'});
-});
-
+// Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+} else {
+  app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Support Desk API'});
+  });
+}
 
 app.use(errorHandler);
 
